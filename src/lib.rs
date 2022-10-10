ec57f4bfe961e9aa5c8a482a6e33b4c61de944cd
Cyril Plisko <cyril.plisko@mountall.com>
2022-10-10 08:36:59 +0300

    Make v1beta1 public

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,21 @@
+#![cfg_attr(feature = "pedantic", warn(clippy::pedantic))]
+#![warn(clippy::use_self)]
+#![warn(clippy::map_flatten)]
+#![warn(clippy::map_unwrap_or)]
+#![warn(clippy::flat_map_option)]
+#![warn(deprecated_in_future)]
+#![warn(future_incompatible)]
+#![warn(noop_method_call)]
+#![warn(unreachable_pub)]
+#![warn(missing_debug_implementations)]
+#![warn(rust_2018_compatibility)]
+#![warn(rust_2021_compatibility)]
+#![warn(rust_2018_idioms)]
+#![warn(unused)]
+#![deny(warnings)]
+
 use k8s_openapi as k8s;
 use k8s_openapi::apimachinery::pkg::apis::meta::v1 as metav1;
 use serde::{Deserialize, Serialize};
 
-mod v1beta1;
+pub mod v1beta1;
